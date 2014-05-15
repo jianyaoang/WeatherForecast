dcb335464d1df2e658f537ac23403c14b2f09524
jianyaoang <jianyaoang1@gmail.com>
2014-05-15 13:02:09 -0400

    removed unwanted code, added Cincinnati weather information

--- WeatherForecast/ViewController.m
+++ WeatherForecast/ViewController.m
@@ -47,6 +47,7 @@
     [self extractingNewYorkJSONData];
     [self extractingSeattleJSONData];
     [self extractingWashingtonDCJSONData];
+    [self extractingCincinnatiJSONData];
 }
 
 -(void)extractingChicagoJSONData
@@ -136,7 +137,6 @@
                 city.fcttext = forecastdayInfo[@"fcttext"];
                 city.fcttext_metric = forecastdayInfo[@"fcttext_metric"];
                 [cwi.forecast addObject:city];
-                NSLog(@"city.forecast 2: %d",city.forecast.count);
             }
             [citiesTableView reloadData];
         }
@@ -282,7 +282,51 @@
     }];
 }
 
-
+-(void)extractingCincinnatiJSONData
+{
+    NSURL *url = [NSURL URLWithString:@"http://api.wunderground.com/api/ac564405ca26fd91/forecast10day/conditions/q/OH/Cincinnati.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        if (connectionError)
+        {
+            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Connection Error" message:@"Unable to retrieve data" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [av show];
+        }
+        else
+        {
+            NSDictionary *citiesWeatherFirstLayer = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+            current_observation = citiesWeatherFirstLayer[@"current_observation"];
+            display_location = current_observation[@"display_location"];
+            
+            forecast = citiesWeatherFirstLayer[@"forecast"];
+            NSDictionary *txt_forecast = forecast[@"txt_forecast"];
+            NSArray *forecastday = txt_forecast[@"forecastday"];
+            
+            CitiesWeatherInformation *cwi = [CitiesWeatherInformation new];
+            cwi.forecast = [[NSMutableArray alloc] initWithCapacity:10];
+            cwi.temp_string = current_observation[@"temperature_string"];
+            cwi.temp_c = [current_observation[@"temp_c"]floatValue];
+            cwi.temp_f = [current_observation[@"temp_f"]floatValue];
+            cwi.city = display_location[@"city"];
+            [allCitiesData addObject:cwi];
+            
+            [cwi.forecast removeAllObjects];
+            
+            for (NSDictionary *forecastdayInfo in forecastday)
+            {
+                City *city = [City new];
+                city.title = forecastdayInfo[@"title"];
+                city.icon = forecastdayInfo[@"icon"];
+                city.period = [forecastdayInfo[@"period"]floatValue];
+                city.fcttext = forecastdayInfo[@"fcttext"];
+                city.fcttext_metric = forecastdayInfo[@"fcttext_metric"];
+                [cwi.forecast addObject:city];
+            }
+            [citiesTableView reloadData];
+        }
+    }];
+}
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
