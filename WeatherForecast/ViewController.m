4e7f383e8e8f32527ce757b78ff717c0655d336e
jianyaoang <jianyaoang1@gmail.com>
2014-05-15 12:41:11 -0400

    added New York and Seattle weather information

--- WeatherForecast/ViewController.m
+++ WeatherForecast/ViewController.m
@@ -21,7 +21,6 @@
     NSMutableArray *allCitiesData;
     NSMutableArray *citiesForecastData;
 }
-
 @end
 
 @implementation ViewController
@@ -34,13 +33,18 @@
     display_location = [NSDictionary new];
     forecast = [NSDictionary new];
 
-    citiesChicagoData = [NSMutableArray new];
-    citiesDallasData = [NSMutableArray new];
-    [self extractingChicagoJSONData];
-    [self extractingDallasJSONData];
-    
     allCitiesData = [NSMutableArray new];
     citiesForecastData = [NSMutableArray new];
+    
+    [self extractingCitiesJSONData];
+}
+
+-(void)extractingCitiesJSONData
+{
+    [self extractingChicagoJSONData];
+    [self extractingDallasJSONData];
+    [self extractingNewYorkJSONData];
+    [self extractingSeattleJSONData];
 }
 
 -(void)extractingChicagoJSONData
@@ -131,6 +135,99 @@
                 city.fcttext_metric = forecastdayInfo[@"fcttext_metric"];
                 [cwi.forecast addObject:city];
                 NSLog(@"city.forecast 2: %d",city.forecast.count);
+            }
+            [citiesTableView reloadData];
+        }
+    }];
+}
+
+-(void)extractingNewYorkJSONData
+{
+    NSURL *url = [NSURL URLWithString:@"http://api.wunderground.com/api/ac564405ca26fd91/forecast10day/conditions/q/NY/New_York.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        if (connectionError)
+        {
+            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Connection Error" message:@"Unable to retrieve data" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [av show];
+        }
+        else
+        {
+            NSDictionary *citiesWeatherFirstLayer = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+            current_observation = citiesWeatherFirstLayer[@"current_observation"];
+            display_location = current_observation[@"display_location"];
+            
+            forecast = citiesWeatherFirstLayer[@"forecast"];
+            NSDictionary *txt_forecast = forecast[@"txt_forecast"];
+            NSArray *forecastday = txt_forecast[@"forecastday"];
+            
+            CitiesWeatherInformation *cwi = [CitiesWeatherInformation new];
+            cwi.forecast = [[NSMutableArray alloc] initWithCapacity:10];
+            cwi.temp_string = current_observation[@"temperature_string"];
+            cwi.temp_c = [current_observation[@"temp_c"]floatValue];
+            cwi.temp_f = [current_observation[@"temp_f"]floatValue];
+            cwi.city = display_location[@"city"];
+            [allCitiesData addObject:cwi];
+            
+            [cwi.forecast removeAllObjects];
+            
+            for (NSDictionary *forecastdayInfo in forecastday)
+            {
+                City *city = [City new];
+                city.title = forecastdayInfo[@"title"];
+                city.icon = forecastdayInfo[@"icon"];
+                city.period = [forecastdayInfo[@"period"]floatValue];
+                city.fcttext = forecastdayInfo[@"fcttext"];
+                city.fcttext_metric = forecastdayInfo[@"fcttext_metric"];
+                [cwi.forecast addObject:city];
+            }
+            [citiesTableView reloadData];
+
+        }
+    }];
+}
+
+-(void)extractingSeattleJSONData
+{
+    NSURL *url = [NSURL URLWithString:@"http://api.wunderground.com/api/ac564405ca26fd91/forecast10day/conditions/q/WA/Seattle.json"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError)
+    {
+        if (connectionError)
+        {
+            UIAlertView *av = [[UIAlertView alloc] initWithTitle:@"Connection Error" message:@"Unable to retrieve data" delegate:nil cancelButtonTitle:@"Okay" otherButtonTitles:nil];
+            [av show];
+        }
+        else
+        {
+            NSDictionary *citiesWeatherFirstLayer = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+            current_observation = citiesWeatherFirstLayer[@"current_observation"];
+            display_location = current_observation[@"display_location"];
+            
+            forecast = citiesWeatherFirstLayer[@"forecast"];
+            NSDictionary *txt_forecast = forecast[@"txt_forecast"];
+            NSArray *forecastday = txt_forecast[@"forecastday"];
+            
+            CitiesWeatherInformation *cwi = [CitiesWeatherInformation new];
+            cwi.forecast = [[NSMutableArray alloc] initWithCapacity:10];
+            cwi.temp_string = current_observation[@"temperature_string"];
+            cwi.temp_c = [current_observation[@"temp_c"]floatValue];
+            cwi.temp_f = [current_observation[@"temp_f"]floatValue];
+            cwi.city = display_location[@"city"];
+            [allCitiesData addObject:cwi];
+            
+            [cwi.forecast removeAllObjects];
+            
+            for (NSDictionary *forecastdayInfo in forecastday)
+            {
+                City *city = [City new];
+                city.title = forecastdayInfo[@"title"];
+                city.icon = forecastdayInfo[@"icon"];
+                city.period = [forecastdayInfo[@"period"]floatValue];
+                city.fcttext = forecastdayInfo[@"fcttext"];
+                city.fcttext_metric = forecastdayInfo[@"fcttext_metric"];
+                [cwi.forecast addObject:city];
             }
             [citiesTableView reloadData];
         }
